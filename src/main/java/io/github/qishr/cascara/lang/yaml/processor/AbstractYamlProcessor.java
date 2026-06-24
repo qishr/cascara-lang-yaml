ea285d5153bdd86fc7494b55987cdcf6152885d5
Sandy Dunlop <sandy.dunlop@gmail.com>
2026-06-24 15:20:25 +0100

    Refactored seralization.

--- src/main/java/io/github/qishr/cascara/lang/yaml/processor/AbstractYamlProcessor.java
+++ src/main/java/io/github/qishr/cascara/lang/yaml/processor/AbstractYamlProcessor.java
@@ -9,6 +9,8 @@ import io.github.qishr.cascara.common.util.Properties;
 import io.github.qishr.cascara.lang.yaml.YamlOptions;
 
 public abstract class AbstractYamlProcessor<P extends Processor> implements Processor {
+    static final String YAML_CONTENT_TYPE_STRING = "text/yaml";
+
     static final ContentType YAML_CONTENT_TYPE =
         new ContentType("YAML")
             .withType("text/yaml")
@@ -26,7 +28,7 @@ public abstract class AbstractYamlProcessor<P extends Processor> implements Proc
     public Properties getServiceProperties() {
         if (properties == null) {
             properties = new Properties();
-            properties.set("contentType", "text/yaml");
+            properties.set("contentType", YAML_CONTENT_TYPE_STRING);
         }
         return properties;
     }
---
+++ src/main/java/io/github/qishr/cascara/lang/yaml/processor/YamlFactory.java
@@ -0,0 +1,41 @@
+package io.github.qishr.cascara.lang.yaml.processor;
+
+import io.github.qishr.cascara.common.lang.QuoteStyle;
+import io.github.qishr.cascara.common.lang.processor.AstFactory;
+import io.github.qishr.cascara.common.lang.type.Primitive;
+import io.github.qishr.cascara.lang.yaml.ast.YamlMapEntryNode;
+import io.github.qishr.cascara.lang.yaml.ast.YamlMapNode;
+import io.github.qishr.cascara.lang.yaml.ast.YamlNode;
+import io.github.qishr.cascara.lang.yaml.ast.YamlScalarNode;
+import io.github.qishr.cascara.lang.yaml.ast.YamlSequenceNode;
+
+public class YamlFactory implements AstFactory<YamlNode,YamlScalarNode,YamlSequenceNode,YamlMapNode,YamlMapEntryNode> {
+
+    @Override
+    public YamlScalarNode createScalarNode(Object primitiveValue) {
+        // TODO: Should this parameter be called:
+        // - `primitiveValue` (used in AstNode), or
+        // - `jvmInstance` (used in Serializer) ?
+        // Check coding standards and update if need be.
+        return new YamlScalarNode(primitiveValue);
+    }
+
+    @Override
+    public YamlScalarNode createScalarNode(Primitive primitive) {
+        return YamlScalarNode.fromPrimitive(primitive);
+    }
+
+    @Override
+    public YamlSequenceNode createSequenceNode() {
+        return new YamlSequenceNode();
+    }
+
+    @Override
+    public YamlMapNode createMapNode() {
+        return new YamlMapNode();
+    }
+
+    public YamlScalarNode createScalarNode(Object key, QuoteStyle quoteStyle) {
+        return new YamlScalarNode(key, quoteStyle);
+    }
+}
