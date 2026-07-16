6354dd6a8baac671817326f254f6dd17dc33dc4a
Sandy Dunlop <sandy.dunlop@gmail.com>
2026-07-16 23:45:29 +0100

    License Update

--- src/main/java/io/github/qishr/cascara/lang/yaml/internal/YamlStreamEngine.java
+++ src/main/java/io/github/qishr/cascara/lang/yaml/internal/YamlStreamEngine.java
@@ -1,17 +1,54 @@
-package io.github.qishr.cascara.lang.yaml.processor;
+// # License & Terms
+//
+// This file is part of **Cascara**.
+//
+// **Cascara** is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// ---
+//
+// ## Special Runtime Exception
+//
+// As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent modules,
+// and to copy and distribute the resulting executable under terms of your
+// choice, provided that you also meet, for each linked independent module,
+// the terms and conditions of the license of that module.
+//
+// An independent module is a module which is not derived from or based on
+// this library. If you modify this library, you may extend this exception
+// to your version of the library, but you are not obligated to do so. If
+// you do not wish to do so, delete this exception statement from your
+// version.
+
+
+package io.github.qishr.cascara.lang.yaml.internal;
 
 import io.github.qishr.cascara.common.diagnostic.Reporter;
 import io.github.qishr.cascara.common.lang.streaming.StreamingEventType;
 import io.github.qishr.cascara.common.lang.exception.ParserException;
 import io.github.qishr.cascara.common.lang.token.Token;
 import io.github.qishr.cascara.lang.yaml.exception.YamlDiagnosticCode;
+import io.github.qishr.cascara.lang.yaml.processor.YamlTokenizer;
+import io.github.qishr.cascara.lang.yaml.streaming.YamlStreamingEvent;
 import io.github.qishr.cascara.lang.yaml.token.YamlTokenType;
 
 import java.io.InputStream;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-class YamlStreamEngine {
+public class YamlStreamEngine {
     private final YamlTokenizer tokenizer;
     private final Deque<Integer> indentStack = new ArrayDeque<>();
     private final Deque<StreamingEventType> contextStack = new ArrayDeque<>();
@@ -28,7 +65,7 @@ class YamlStreamEngine {
     private boolean insideBlockScalar = false;
     private final StringBuilder blockScalarBuffer = new StringBuilder();
 
-    YamlStreamEngine(InputStream input, Reporter reporter, boolean includeComments) {
+    public YamlStreamEngine(InputStream input, Reporter reporter, boolean includeComments) {
         this.tokenizer = new YamlTokenizer().setReporter(reporter);
         this.tokenizer.open(input);
         this.indentStack.push(0);
@@ -40,12 +77,12 @@ class YamlStreamEngine {
         return this;
     }
 
-    boolean hashNextEvent() {
+    public boolean hashNextEvent() {
         // If we are already done, do not claim to have events
         return !isDocumentEnded;
     }
 
-    YamlStreamingEvent nextEvent() throws ParserException {
+    public  YamlStreamingEvent nextEvent() throws ParserException {
         if (isDocumentEnded) return null; // Or handle as appropriate
 
         if (!rootOpened) {
--- src/main/java/io/github/qishr/cascara/lang/yaml/streaming/YamlStreamingEvent.java
+++ src/main/java/io/github/qishr/cascara/lang/yaml/streaming/YamlStreamingEvent.java
@@ -33,36 +33,46 @@
 // version.
 
 
-module cascara.lang.yaml {
-    requires transitive cascara.common;
+package io.github.qishr.cascara.lang.yaml.streaming;
 
-    exports io.github.qishr.cascara.lang.yaml;
-    exports io.github.qishr.cascara.lang.yaml.annotation;
-    exports io.github.qishr.cascara.lang.yaml.ast;
-    exports io.github.qishr.cascara.lang.yaml.exception;
-    exports io.github.qishr.cascara.lang.yaml.processor;
-    exports io.github.qishr.cascara.lang.yaml.token;
-    exports io.github.qishr.cascara.lang.yaml.util;
+import io.github.qishr.cascara.common.lang.streaming.StreamingEvent;
+import io.github.qishr.cascara.common.lang.streaming.StreamingEventType;
 
-    opens io.github.qishr.cascara.lang.yaml.annotation;
-    opens io.github.qishr.cascara.lang.yaml.ast;
-    opens io.github.qishr.cascara.lang.yaml.exception;
-    opens io.github.qishr.cascara.lang.yaml.processor to cascara.common;
-    opens io.github.qishr.cascara.lang.yaml.token;
+public class YamlStreamingEvent implements StreamingEvent {
+	private final int lineNumber;
+	private final int columnNumber;
+	private final String content;
+	private final StreamingEventType type;
 
-    provides io.github.qishr.cascara.common.lang.processor.AstConverter
-        with io.github.qishr.cascara.lang.yaml.processor.YamlConverter;
+    public YamlStreamingEvent(int lineNumber, int columnNumber, StreamingEventType type, String content) {
+		this.lineNumber = lineNumber;
+		this.columnNumber = columnNumber;
+		this.type = type;
+		this.content = content != null ? content : "";
+    }
 
-    provides io.github.qishr.cascara.common.lang.processor.Emitter
-        with io.github.qishr.cascara.lang.yaml.processor.YamlEmitter;
+	@Override
+	public StreamingEventType getType() {
+		return type;
+	}
 
-    provides io.github.qishr.cascara.common.lang.processor.AstParser
-        with io.github.qishr.cascara.lang.yaml.processor.YamlAstParser;
+	@Override
+	public String getContent() {
+		return content;
+	}
 
-    provides io.github.qishr.cascara.common.lang.processor.Tokenizer
-        with io.github.qishr.cascara.lang.yaml.processor.YamlTokenizer;
+	@Override
+	public long getLineNumber() {
+		return lineNumber;
+	}
 
-    // It also works this way...
-    // provides io.github.qishr.cascara.common.service.ServiceProvider
-    //     with io.github.qishr.cascara.lang.yaml.processor.YamlTokenizer;
+	@Override
+	public long getColumnNumber() {
+		return columnNumber;
+	}
+
+	@Override
+    public String toString() {
+        return String.format("[%d:%d] %s -> %s", lineNumber, columnNumber, type, content.isEmpty() ? "EMPTY" : content);
+    }
 }
